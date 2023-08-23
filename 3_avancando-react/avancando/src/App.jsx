cb6a6163ad9917f42a124a548c6df0dbff29de4f
Lucas Fagundes Carneiro <>
2023-08-22 21:42:33 -0300

    secao 3 finalizada

--- 3_avancando-react/avancando/src/App.jsx
+++ 3_avancando-react/avancando/src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css'
+//images
+import City from "./assets/City.jpg";
+import { useState } from 'react';
+
 //components
 import ManageData from './components/ManageData';
 import RenderList from './components/RenderList';
+import ConditionalRender from './components/ConditionalRender';
+import CarDetails from './components/CarDetails';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
+import ShowUserName from './components/ShowUserName';
+import Message from './components/Message';
+import ChangeMessageState from './components/ChangeMessageState';
 
-//images
-import City from "./assets/City.jpg";
 
 function App() {
+
+  const name = "Matheus";
+  const [userName] = useState("Maria")
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Amarela", newCar: false, km: 10000},
+    {id: 2, brand: "Ford", color: "Azul", newCar: true, km: 0},
+    {id: 3, brand: "Fiat", color: "Preto", newCar: false, km: 2343}
+  ]
+
+  function ShowMessage() {
+    console.log("Evento do componente pai")
+  }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  }
 
   return (
     <div className="App">
@@ -17,6 +45,37 @@ function App() {
       </div>
       <ManageData/>
       <RenderList/>
+      <ConditionalRender/>
+          {/* Props */}
+      <ShowUserName name = {userName}/> 
+          {/* Destructuring */}
+      <CarDetails brand= "Fiat" km={55000} color="preto" newCar= {false}/>
+          {/*Reaproveitando*/}
+      <CarDetails brand= "Ford" km={0} color="Azul" newCar= {true}/>
+      <CarDetails brand= "Fiat" km={5000} color="prata" newCar= {false}/>
+          {/* Loop em array de objetos */}
+      {cars.map((car)=>(
+        <CarDetails
+          key={car.id}
+          brand = {car.brand} 
+          km = {car.km} 
+          color = {car.color} 
+          newCar = {car.newCar} />
+      ))}
+          {/* Fragment */}
+      <Fragment propFragment = "teste" />
+          {/* Children */}
+      <Container myValue = "valor 1">
+        <p>Este é o conteudo!</p>
+      </Container>
+      <Container myValue = "valor 2">
+        <h4>Testando container</h4>
+      </Container>
+          {/* Executar funcao passada pelo props */}
+      <ExecuteFunction myFunction={ShowMessage}/>
+          {/* State Lift */}
+      <Message msg={message}/>
+      <ChangeMessageState handleMessage={handleMessage}/>
     </div>
   );
 };
