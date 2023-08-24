5146c4ae953e4bdc093a66c9364d0261b62c4b7e
Lucas Fagundes Carneiro <>
2023-08-24 16:03:14 -0300

    secao 4 challenge css

---
+++ 4_css-react/challenge/src/components/Cars.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from "./Cars.module.css"
+
+const Cars = ({brand ,model ,year ,km}) => {
+
+  return (
+    <div>
+          <div className = {style.carCard}>
+          <h2>{brand}</h2>
+          <p>Modelo: {model} </p>
+          <p>Ano: {year}</p>
+          <p>Km: {km}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Cars
