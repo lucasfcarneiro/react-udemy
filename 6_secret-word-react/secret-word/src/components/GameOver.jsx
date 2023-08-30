9a267001c14988de136e03a8303aa746ac8e28cc
Lucas Fagundes Carneiro <>
2023-08-30 20:56:36 -0300

    secret word project finished

--- 6_secret-word-react/secret-word/src/components/GameOver.jsx
+++ 6_secret-word-react/secret-word/src/components/GameOver.jsx
@@ -1,9 +1,12 @@
 import "./GameOver.css"
 
-const GameOver = () => {
+const GameOver = ({retry, score}) => {
+
   return (
     <div>
-
+        <h1>Fim de jogo</h1>
+        <h2>A sua pontuação foi <span>{score}</span></h2>
+        <button onClick={retry} >Reiniciar</button>
     </div>
   )
 }
--- 6_secret-word-react/secret-word/src/components/StartScreen.jsx
+++ 6_secret-word-react/secret-word/src/components/StartScreen.jsx
@@ -1,11 +1,11 @@
 import "./StartScreen.css"
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className="start">
         <h1>Secret Word</h1>
         <p>Clique no botão para começar a jogar</p>
-        <button>Começar a jogar</button>
+        <button onClick={startGame}>Começar a jogar</button>
     </div>
   )
 }
