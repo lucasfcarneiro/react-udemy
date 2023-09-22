20657edb21d02de6e8783eaa76c712b27ac17f68
Lucas Fagundes Carneiro <>
2023-09-22 16:40:58 -0300

    Navbar and footer

--- 10_miniblog/miniblog/src/App.jsx
+++ 10_miniblog/miniblog/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+//pages
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
