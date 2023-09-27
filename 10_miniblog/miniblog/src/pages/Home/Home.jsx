24494a0a61b98d57594cf572762197563e1fa989
Lucas Fagundes Carneiro <>
2023-09-27 17:02:28 -0300

    Home css

--- 10_miniblog/miniblog/src/pages/Home/Home.jsx
+++ 10_miniblog/miniblog/src/pages/Home/Home.jsx
@@ -1,9 +1,38 @@
+//CSS
 import styles from "./Home.module.css"
 
+//hooks
+import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react"
+
+//components
+
 const Home = () => {
+
+  const [query, setQuery] = useState("")
+  const [posts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)} />
+        <button className="button button-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>posts</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Nao foram encontrados posts</p>
+            <Link to="/posts/create" className="button">Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
