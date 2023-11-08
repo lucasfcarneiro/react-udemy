fb41892f1db6e91aaf86ca75f4819088aed93073
Lucas Fagundes Carneiro <>
2023-11-08 16:36:38 -0300

    buttons at dashboard implemented

--- 10_miniblog/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ 10_miniblog/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -10,7 +10,15 @@ const Dashboard = () => {
   const { user } = useAuthValue()
   const uid = user.uid
 
-  const {documents:posts, loading} = useFetchDocuments("posts", null, uid)
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
+
+  const deleteDocument = (id) => {
+
+  }
+
+  if(loading){
+    return <p>Carregando...</p>
+  }
 
   return (
     <div>
@@ -24,11 +32,21 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>
-            <p>Tem posts!</p>
-        </div>
+        <>
+          <div>
+            <span>Títulos</span>
+            <span>Ações</span>
+          </div>
+          {posts && posts.map((post) => <div key={post.id}>
+            <p>{post.title}</p>
+            <div>
+              <Link to={`/posts/${post.id}`} className="button button-outline">Ver</Link>
+              <Link to={`/posts/edit${post.id}`} className="button button-outline">Editar</Link>
+              <button onClick={() => deleteDocument(post.id)} className="button button-outline button-danger">Excluir</button>
+            </div>
+          </div>)}
+        </>
       )};
-      {posts && posts.map((post)=> <h3>{post.title}</h3>)}
     </div>
   );
 };
