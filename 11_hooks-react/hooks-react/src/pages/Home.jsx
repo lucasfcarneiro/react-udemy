68caff44193518c4313ac33cdfb334452ff0ffb5
Lucas Fagundes Carneiro <>
2023-12-01 17:15:00 -0300

    Hook useEffect

--- 11_hooks-react/hooks-react/src/pages/Home.jsx
+++ 11_hooks-react/hooks-react/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HookUseState from '../components/HookUseState'
 import HookUserReducer from '../components/HookUserReducer'
+import HookUseEffect from '../components/HookUseEffect'
 
 const Home = () => {
   return (
     <div>
       <HookUseState/>
       <HookUserReducer/>
+      <HookUseEffect/>
     </div>
   )
 }
