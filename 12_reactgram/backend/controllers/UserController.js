0b56ef8410a17d786d625e67bee22813482de5ae
Lucas Fagundes Carneiro <>
2023-12-19 16:06:12 -0300

    user validation

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -6,15 +6,72 @@ const jwt = require("jsonwebtoken")
 const jwtSecret = process.env.JWT_SECRET
 
 //generate user token
-const generateToken = (id) =>{
-    return jwt.sign({id}, jwtSecret, {expiresIn: "7d",})
+const generateToken = (id) => {
+    return jwt.sign({ id }, jwtSecret, { expiresIn: "7d", })
 }
 
 //Register user and sign in 
-const register = async (req, res) =>{
-    res.send("Registro")
+const register = async (req, res) => {
+
+    try {
+        const { name, email, password } = req.body
+
+        //check if user exists
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return res.status(422).json({ errors: ["Email ja cadastrado"] })
+        }
+
+        //Generate password hash
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        //Create user
+        const newUser = await User.create({
+            name,
+            email,
+            password: passwordHash
+        })
+        //if user was created successfully, return the token
+        return res.status(201).json({
+            _id: newUser._id,
+            token: generateToken(newUser._id)
+        })
+    }
+    catch (error) {
+        if (newUser) {
+            return res.status(422).json({ errors: ["Houve um error, por favor tente mais tarde"] })
+        }
+    }
+}
+
+//sign user in
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    //check if user exist
+    if (!user) {
+        res.status(404).json({ errors: "Usuario nao encontrado" })
+        return
+    }
+
+    //check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({ errors: ["Senha invalida"] })
+        return
+    }
+
+    //return user with token
+    return res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id)
+    })
 }
 
 module.exports = {
-    register,
+    register, login
 }
--- 12_reactgram/backend/middlewares/userValidations.js
+++ 12_reactgram/backend/middlewares/userValidations.js
@@ -29,6 +29,18 @@ const userCreateValidation = () => {
     ]
 }
 
+const loginValidation = () => {
+    return [
+        body("email")
+            .isString()
+            .withMessage("O email é obrigatorio")
+            .isEmail()
+            .withMessage("Insira um email valido"),
+        body("password")
+            .isString().withMessage("A senha é obrigatoria")
+    ]
+}
+
 module.exports = {
-    userCreateValidation,
+    userCreateValidation,loginValidation
 }
