a13a76d219fb3347c6d5116881a164e3446f8c7c
Lucas Fagundes Carneiro <>
2024-03-27 15:59:49 -0300

    update photo done

--- 12_reactgram/backend/controllers/PhotoController.js
+++ 12_reactgram/backend/controllers/PhotoController.js
@@ -97,10 +97,40 @@ const getPhotoById = async(req,res) => {
     res.status(200).json(photo)
 }; 
 
+//update a photo
+const updatePhoto = async(req,res) => {
+    const {id} = req.params
+    const {title} = req.body
+
+    const reqUser = req.user
+
+    const photo = await Photo.findById(new mongoose.Types.ObjectId(id))
+
+    //check if photo exists
+    if(!photo){
+        res.status(404).json({errors:["Foto nao encontrada"]})
+        return
+    }
+    
+    //check if photo belongs to user
+    if(!photo.userId.equals(reqUser._id)){
+        res.status(422).json({ errors: ["Ocorreu um erro."] })
+        return
+    }
+
+    if(title){
+        photo.title = title
+    }
+
+    await photo.save()
+    res.status(200).json({photo, message: "Foto atualizada com sucesso."})
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
     getPhotoById,
+    updatePhoto,
 }
