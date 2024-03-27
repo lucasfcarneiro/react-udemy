5e713832986f7c63d83bf2cfe670381a4c4a9ef3
Lucas Fagundes Carneiro <>
2024-03-27 12:43:44 -0300

    photo insert done

--- 12_reactgram/backend/controllers/PhotoController.js
+++ 12_reactgram/backend/controllers/PhotoController.js
@@ -1,5 +1,8 @@
 const Photo = require("../models/Photo")
+const User = require("../models/User")
+
 const mongoose = require("mongoose")
+
 
 //insert a photo, with an user related to it
 const insertPhoto = async (req, res) => {
@@ -7,11 +10,29 @@ const insertPhoto = async (req, res) => {
     const { title } = req.body
     const image = req.file.filename
 
-    console.log(req.body)
-    res.send("photo inserted")
+    const reqUser = req.user
+    const user = await User.findById(reqUser._id)
+
+    //create a photo
+    const newPhoto = await Photo.create({
+        image,
+        title,
+        userId: user._id,
+        userName: user.name,
+        //subtitle
+    });
+
+    //If photo was created successfully, return data
+    if (!newPhoto) {
+        res.status(422).json({
+            errors: ["Houve um problema, tente mais tarde"]
+        })
+    }
+    res.status(200).json(newPhoto)
+    
 }
 
 module.exports = {
-        insertPhoto,
-        
-    }
+    insertPhoto,
+
+}
