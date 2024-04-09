e63812ba0be3dbf3aaeba3081da2e3e293710f5d
Lucas Fagundes Carneiro <>
2024-04-08 23:55:39 -0300

    register page

--- 12_reactgram/frontend/src/pages/Auth/Register.jsx
+++ 12_reactgram/frontend/src/pages/Auth/Register.jsx
@@ -1,9 +1,29 @@
 import "./Auth.css"
 
+//Components
+import {Link} from 'react-router-dom'
+
+//Hooks
+import {useState, useEffect} from 'react'
+
 const Register = () => {
+
+const handleSubmit = (e) => {
+e.preventDefault()
+}
+
   return (
     <div>
-      <h1>Register</h1>
+      <h2>Register</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Nome"/>
+      <input type="email" placeholder="E-mail"/>
+      <input type="password" placeholder="Senha"/>
+      <input type="password" placeholder="Confirme sua senha"/>
+      <input type="submit" placeholder="Cadastrar"/>
+      </form>
+      <p>Já tem conta? <Link to="/login" >Clique aqui.</Link></p>
     </div>
   )
 }
