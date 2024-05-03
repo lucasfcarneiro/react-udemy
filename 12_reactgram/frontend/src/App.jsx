4e72e8f354e4d6e26897c1dc0ac3813e4823d0bb
Lucas Fagundes Carneiro <>
2024-05-03 16:34:24 -0300

    edit profile page created

--- 12_reactgram/frontend/src/App.jsx
+++ 12_reactgram/frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 import Home from './pages/Home/Home'
+import EditProfile from './pages/EditProfile/EditProfile'
 
 function App() {
 
@@ -29,9 +30,18 @@ function App() {
         <Navbar />
         <div className='container'>
           <Routes>
-            <Route path='/' element={auth ? <Home /> : <Navigate to="/login" />} />
-            <Route path='/login' element={!auth ? <Login /> : <Navigate to="/" />} />
-            <Route path='/register' element={!auth ? <Register /> : <Navigate to="/" />} />
+            <Route
+              path='/'
+              element={auth ? <Home /> : <Navigate to="/login" />} />
+            <Route
+              path='/profile'
+              element={auth ? <EditProfile /> : <Navigate to="/login" />} />
+            <Route
+              path='/login'
+              element={!auth ? <Login /> : <Navigate to="/" />} />
+            <Route
+              path='/register'
+              element={!auth ? <Register /> : <Navigate to="/" />} />
           </Routes></div>
         <Footer />
       </BrowserRouter>
---
+++ 12_reactgram/frontend/src/pages/EditProfile/EditProfile.jsx
@@ -0,0 +1,38 @@
+import "./EditProfile.css"
+
+const EditProfile = () => {
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+}
+
+  return (
+    <div id="edit-profile">
+        <h2>Edite seus dados</h2>
+        <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre voce. </p>
+        {/* Preview image */}
+
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Nome" />
+            <input type="email" placeholder="E-mail" disabled />
+            <label>
+                <span>Imagem do Perfil</span>
+                <input type="file" />
+            </label>
+            <label>
+                <span>Bio:</span>
+                <input type="text" placeholder="Descricao do perfil"/>
+            </label>
+            <label>
+                <span>Quer alterar sua senha?</span>
+                <input type="password" placeholder="Digite sua nova senha"/>
+            </label>
+
+            <input type="submit" value="Atualizar"/>
+
+        </form>
+    </div>
+  )
+}
+
+export default EditProfile
