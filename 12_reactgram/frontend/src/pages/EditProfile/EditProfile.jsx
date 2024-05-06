9cb3725091c17f8019c7639fe822cf025bc7e378
Lucas Fagundes Carneiro <>
2024-05-06 17:23:35 -0300

    fill user data at editProfile page

--- 12_reactgram/frontend/src/pages/EditProfile/EditProfile.jsx
+++ 12_reactgram/frontend/src/pages/EditProfile/EditProfile.jsx
@@ -1,38 +1,92 @@
 import "./EditProfile.css"
 
+import { uploads } from "../../utils/config"
+
+//Hooks
+import { useEffect, useState } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+
+//Redux
+import { profile, resetMessage } from '../../slices/userSlice'
+
+//components
+import Message from '../../components/Message'
+
 const EditProfile = () => {
+    const dispatch = useDispatch()
+    const { user, Message, error, loading } = useSelector((state) => state.user)
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-}
+    //state
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [profileImage, setProfileImage] = useState("")
+    const [bio, setBio] = useState("")
+    const [previewImage, setPreviewImage] = useState("")
 
-  return (
-    <div id="edit-profile">
-        <h2>Edite seus dados</h2>
-        <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre voce. </p>
-        {/* Preview image */}
+    //Load user data
+    useEffect(() => {
+        dispatch(profile())
+    }, [dispatch])
 
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Nome" />
-            <input type="email" placeholder="E-mail" disabled />
-            <label>
-                <span>Imagem do Perfil</span>
-                <input type="file" />
-            </label>
-            <label>
-                <span>Bio:</span>
-                <input type="text" placeholder="Descricao do perfil"/>
-            </label>
-            <label>
-                <span>Quer alterar sua senha?</span>
-                <input type="password" placeholder="Digite sua nova senha"/>
-            </label>
+    //Fill form with user data
+    useEffect(() =>{
 
-            <input type="submit" value="Atualizar"/>
+        if(user){
+            setName(user.name)
+            setEmail(user.email)
+            setBio(user.bio)
+        }
+    }, [user])
 
-        </form>
-    </div>
-  )
+    console.log(user)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div id="edit-profile">
+            <h2>Edite seus dados</h2>
+            <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre voce. </p>
+            {/* Preview image */}
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name || ""} />
+                <input
+                    type="email"
+                    placeholder="E-mail" disabled
+                    value={email} />
+                <label>
+                    <span>Imagem do Perfil</span>
+                    <input type="file" />
+                </label>
+                <label>
+                    <span>Bio:</span>
+                    <input
+                        type="text"
+                        placeholder="Descricao do perfil"
+                        onChange={(e) => setBio(e.target.value)}
+                        value={bio || ""} />
+                </label>
+                <label>
+                    <span>Quer alterar sua senha?</span>
+                    <input
+                        type="password"
+                        placeholder="Digite sua nova senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password || ""} />
+                </label>
+
+                <input type="submit" value="Atualizar" />
+
+            </form>
+        </div>
+    )
 }
 
 export default EditProfile
